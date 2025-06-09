ed26dc7bc8431ea0c965de1786ab07117dcb77c1
rlysleepynick <rlysleepynick@rlySUSEpc.muridae-powan.ts.net>
2025-06-09 17:10:38 +0900

    edit:nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,35 @@
+-- lazy.nvim のパス
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+-- lazy.nvim が存在しない場合 clone
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+
+-- runtimepath に追加
+vim.opt.rtp:prepend(lazypath)
+
+-- プラグイン一覧
+require("lazy").setup({
+  -- ここに使いたいプラグインを書く
+  "nvim-lualine/lualine.nvim",         -- ステータスライン
+  "nvim-treesitter/nvim-treesitter",   -- 高速構文解析
+  "nvim-telescope/telescope.nvim",     -- Fuzzy Finder
+  "neovim/nvim-lspconfig",             -- LSP
+  "Mofiqul/dracula.nvim",
+})
+
 -- シンタックスハイライトをオンにする
 vim.cmd('syntax on')
 
 -- カラースキーム
--- vim.cmd('colorscheme molokai')
+-- vim.cmd('colorscheme dracula')
 vim.o.termguicolors = true -- これで t_Co=256 は不要
 
 -- 基本設定
